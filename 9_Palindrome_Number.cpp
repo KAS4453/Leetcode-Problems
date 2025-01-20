0a1ee7e1ae395a7252aaa3b3dc588bb0000e7ccb
Kunwar Arpit Singh <70731520+KAS4453@users.noreply.github.com>
2025-01-20 23:30:44 +0530

    Create 9_Palindrome_Number.cpp

---
+++ 9_Palindrome_Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        
+        int rev_x = 0;
+        while (x > rev_x) {
+            rev_x = rev_x * 10 + x % 10;
+            x = x / 10;
+        }
+        return (x == rev_x || x == rev_x / 10);
+    }
+};
